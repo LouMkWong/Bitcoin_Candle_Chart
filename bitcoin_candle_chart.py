4c283e03c267bfa8f5b5e34714eb9fbbd56d366b
LouMkWong <86810684+LouMkWong@users.noreply.github.com>
2021-08-29 22:24:00 -0400

    Update bitcoin_candle_chart.py

--- bitcoin_candle_chart.py
+++ bitcoin_candle_chart.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[9]:
-
 
 get_ipython().system('pip install pycoingecko')
 get_ipython().system('pip install plotly')
 get_ipython().system('pip install mplfinance')
-
-
-
-
-
-# In[10]:
-
 
 import pandas as pd
 import numpy as np
@@ -24,47 +15,28 @@ import datetime
 from pycoingecko import CoinGeckoAPI
 from mplfinance.original_flavor import candlestick2_ohlc
 
-
-# In[11]:
-
-
 cg = CoinGeckoAPI()
 
+#getting data from coingecko
 bitcoin_data = cg.get_coin_market_chart_by_id(id='bitcoin', vs_currency='usd', days=30)
-
-
-# In[13]:
-
 
 bitcoin_price_data = bitcoin_data['prices']
 
 bitcoin_price_data[0:5]
 
-
-# In[14]:
-
-
+#data into panda dataframe
 data = pd.DataFrame(bitcoin_price_data, columns=['TimeStamp', 'Price'])
 data
 
-
-# In[15]:
-
-
+#convert timestamp into readable time
 data['date'] = data['TimeStamp'].apply(lambda d: datetime.date.fromtimestamp(d/1000.0))
 data
 
-
-# In[16]:
-
-
+#Grouping data 
 candlestick_data = data.groupby(data.date, as_index=False).agg({"Price": ['min', 'max', 'first', 'last']})
 candlestick_data
 
-
-# In[17]:
-
-
+#Drawing candlestick chart
 fig = go.Figure(data=[go.Candlestick(x=candlestick_data['date'],
                 open=candlestick_data['Price']['first'], 
                 high=candlestick_data['Price']['max'],
